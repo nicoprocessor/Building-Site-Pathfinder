a03d538ee00627eb10938ae02e07b00d3066ad3a
Nicola Onofri <nicola.onofri@gmail.com>
2018-04-29 02:05:33 +0200

    Initial commit, basic implementation of A*

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+# run here
+if __name__ == '__main__':
+	app.run(port=4995)
