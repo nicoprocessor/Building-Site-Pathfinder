e2d9c360777c8667e75ae4dacb2f8c9d2b238c7a
Nicola Onofri <nicola.onofri@gmail.com>
2018-04-30 05:08:44 +0200

    Moving to CheckedPriorityQueue

--- models/path_finder.py
+++ models/path_finder.py
@@ -1,19 +1,22 @@
 # import timeit
 import math
+import random
+from queue import PriorityQueue
 
-cols = 5
-rows = 5
+cols = 10
+rows = 10
 grid = [None] * rows  # [[]] 2D mesh
+
 incremental_cost = 1
+spot_type_traversable = "traversable"
+spot_type_obstacle = "obstacle"
 
 type_conversion = {
-    "traversable": 'o',
-    "obstacle": 'x',
+    spot_type_traversable: 'o',
+    spot_type_obstacle: 'x',
     "current": '$',
     "path": '0'
 }
-
-spot_types = ["traversable", "obstacle"]
 
 
 def manhattan_distance(a, b):
@@ -30,61 +33,24 @@ def eval_heuristic(a, b):
     return manhattan_distance(a, b)
 
 
-def display_grid():
-    """Print the pathfinding grid on the console using conversion table characters"""
-    for i in range(-1, rows):
-        if i == -1:
-            print('', end='\t')
-            for j in range(cols):
-                print('{}'.format(j), end='\t')
-        else:  # i != 0
-            print('\n{}'.format(i), end='\t')
-            for j in range(cols):
-                print(type_conversion[grid[i][j].__repr__()], end='\t')
-
-
 def a_star():
     """A* pathfinding algorithm"""
     path = []
-    open_set = []
+    open_queue = CheckablePriorityQueue()
     closed_set = []
-
-    # init spots
-    for i in range(rows):
-        grid[i] = [None] * cols
-        for j in range(cols):
-            grid[i][j] = Spot(i, j, "traversable")
-
-    # fill neighbors
-    for i in range(rows):
-        for j in range(cols):
-            grid[i][j].add_neighbors()
 
     # init pathfinding - these can be randomized
     start_spot = grid[0][0]
     end_spot = grid[rows - 1][cols - 1]
-    open_set.append(start_spot)
+    open_queue.put(start_spot)
 
     # A* main loop
     while True:
-        if len(open_set) > 0:
+        if open_queue.qsize() > 0:
+            # Best next option
+            current_spot = open_queue.queue[0]
 
-            # Best next option - the one with lowest f (cost)
-            winner_index = 0
-            for i in range(len(open_set)):
-                if open_set[i].f < open_set[winner_index]:
-                    winner_index = i
-
-                # If we have a tie according to the standard heuristic
-                # select the one with the longest known path (closer to goal)
-                elif open_set[i] == open_set[winner_index]:
-                    if open_set[i].g < open_set[winner_index].g:
-                        winner_index = i
-
-            # update current spot
-            current_spot = open_set[winner_index]
-
-            if open_set[winner_index] == end_spot:
+            if current_spot == end_spot:
                 # Found destination, start backtracking to find path to starting spot
                 temp = current_spot
                 path.append(current_spot)
@@ -93,34 +59,30 @@ def a_star():
                     path.append(temp.previous)
                     temp = temp.previous
 
-                    # #display path on the console
-                    # for count, spot in enumerate(path):
-                    #     print('{} {}'.format(count,spot.__str__()))
-
                 print("Path found!")
                 return True, path
 
             else:
-                # move the best option to open set to closed set
+                # move the best option from open set to closed set
                 closed_set.append(current_spot)
-                open_set.remove(current_spot)
+                open_queue.get()
 
                 # check all the neighbors
                 neighbors = current_spot.neighbors
                 for current_neighbor in neighbors:
-                    # check if neighbor hasn't been visited yet
-                    if current_neighbor not in closed_set:
+                    # check if neither the neightbor hasn't been visited yet nor it's an obstacle
+                    if current_neighbor.spot_type != spot_type_obstacle and current_neighbor not in closed_set:
                         # I could have reached the next spot by following a less efficient path.
                         temp_g = current_spot.g + eval_heuristic(current_neighbor, current_spot)
 
-                        if current_neighbor not in open_set:
-                            open_set.append(current_neighbor)
-                        elif temp_g >= current_neighbor.g:
-                            # current neighbor is not a in a better path
-                            continue
+                        # check if current neighbor is in a better path then the previous one or if it's the first time we visit this spot
+                        if temp_g < current_neighbor.g or current_neighbor.g == 10e+6:
+                            current_neighbor.g = temp_g
+
+                        if not open_queue.__contains__(current_neighbor):
+                            open_queue.put(current_neighbor)
 
                         # update neighbor status
-                        current_neighbor.g = temp_g
                         current_neighbor.h = eval_heuristic(current_neighbor, end_spot)
                         current_neighbor.f = current_neighbor.g + current_neighbor.h
                         current_neighbor.previous = current_spot
@@ -133,9 +95,9 @@ def a_star():
 class Spot(object):
     """A traversable spot on the grid"""
 
-    def __init__(self, x, y, spot_type="traversable"):
-        self.f = 0  # cost function
-        self.g = 0  # actual cost from the starting node
+    def __init__(self, x, y, spot_type=spot_type_traversable):
+        self.f = 1e+6  # cost function
+        self.g = 1e+6  # actual cost from the starting node
         self.h = 0  # heuristic - educated guess
         self.x = x
         self.y = y
@@ -143,8 +105,13 @@ class Spot(object):
         self.previous = None  # previous node in the path
         self.spot_type = spot_type
 
+    def __lt__(self, other):
+        self_priority = (self.f, self.h)
+        other_priority = (other.f, other.h)
+        return self_priority > other_priority
+
     def __repr__(self):
-        return str(self.spot_type)
+        return "Spot at x:" + str(self.x) + " y:" + str(self.y)
 
     def __str__(self):
         return "Spot at x:" + str(self.x) + " y:" + str(self.y)
@@ -163,26 +130,39 @@ class Spot(object):
             self.neighbors.append(grid[self.x][self.y - 1])
 
 
+class CheckablePriorityQueue(PriorityQueue):
+    """Extension of PriorityQueue class, add the functionality of membership test"""
+
+    def __contains__(self, item):
+        with self.mutex:
+            return item in self.queue
+
+
 # main
 if __name__ == '__main__':
     # init spots
+    for i in range(rows):
+        grid[i] = [None] * cols
+        for j in range(cols):
+            grid[i][j] = Spot(i, j, spot_type_traversable)
+
     # for i in range(rows):
-    #     grid[i] = [None]*cols
-    #     for j in range(cols):
-    #         grid[i][j] = Spot(i, j, "traversable")
+    #     if i != 4:
+    #         grid[i][cols-i-1] = Spot(i, i, spot_type_obstacle)
 
-    # # fill neighbors
-    # for i in range(rows):
-    #     for j in range(cols):
-    #         grid[i][j].add_neighbors()
+    # for i in range(20):
+    #     rand_x = random.randint(1, rows-1)
+    #     rand_y = random.randint(1, cols-1)
+    #     grid[rand_x][rand_y] = Spot(rand_x, rand_y, spot_type_obstacle)
 
-    # for count, spot in enumerate(grid):
-    #     print('{} {}'.format(count, spot.__str__()))
+    # fill neighbors
+    for i in range(rows):
+        for j in range(cols):
+            grid[i][j].add_neighbors()
 
-    # display_grid()
     print("Pathfinding...")
-    feasible, solution = a_star()
+    feasible, path = a_star()
 
     if feasible:
-        for step, spot_index in enumerate(list(reversed(solution))):
-            print('{} {}'.format(step, solution[spot_index].__str__()))
+        for step, spot_index in enumerate(list(reversed(path))):
+            print('{} {}'.format(step, spot_index.__str__()))
